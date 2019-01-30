2be7ab11c8bb698453bafb73b7ba89d6f02e5ad8
morganpurdy <morganp28@att.net>
2019-01-30 12:24:13 -0700

    add input field for name entry option

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // array of quotes
 //sources: good housekeeping, forbes, huffington post
-var quotes = ["\"You poetic and noble land mermaid.\"", "\"You are the greatest human being ever invented.\"", "\"Your brain is almost as perfect as your face!\"", "\"Oh you perfect sunflower.\"", "\"You beautiful rule-breaking moth.\"", "\"You are a beautiful, talented, brilliant, powerful musk ox.\"", "\"You are a beautiful tropical fish. You’re smart as a whip and you’re cool under pressure.\"", "\"You cunning, pliable, chestnut-haired sunfish.\"", "\"Oh you beautiful, naïve, sophisticated newborn baby.\"", "\"You are an opalescent tree shark.\"", "\"You are a rainbow infused space unicorn.\"", "\"Shut your mouth. You have all the strengths.\"", "\"You are a beautiful, sassy mannequin come to life.\"" ];
+var newName;
+var quotes = [", you poetic and noble land mermaid.", ", you are the greatest human being ever invented.", ", your brain is almost as perfect as your face!\"", ", you perfect sunflower.\"", ", you beautiful rule-breaking moth.\"", ", you are a beautiful, talented, brilliant, powerful musk ox.\"", ", you are a beautiful tropical fish. You’re smart as a whip and you’re cool under pressure.\"", ", you cunning, pliable, chestnut-haired sunfish.\"", ", Oh you beautiful, naïve, sophisticated newborn baby.\"", ", you are an opalescent tree shark.\"", ", you are a rainbow infused space unicorn.\"", ", shut your mouth. You have all the strengths.\"", ", you are a beautiful, sassy mannequin come to life.\"" ];
 
 // button click to generate quote
 document.querySelector('.generate').addEventListener('click', function() {
@@ -10,6 +11,7 @@ var quote = quotes[Math.floor(Math.random() * quotes.length)];
   // make p class visible
  document.querySelector(".quoteDisplay").style.visibility = "visible";
 
-// display quote
-  document.querySelector(".quoteDisplay").textContent = quote;
+// display quote with new name
+  newName = document.querySelector('.new-name').value;
+  document.querySelector(".quoteDisplay").textContent = "\"" + newName + quote;
 });
