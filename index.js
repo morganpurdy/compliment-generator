3fbe04a168039d1bf320f40cc6cbe7e6bc087c50
morganpurdy <morganp28@att.net>
2019-02-01 00:38:22 -0700

    add quote

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // array of quotes
 //sources: good housekeeping, forbes, huffington post
 var newName;
-var quotes = [", you poetic and noble land mermaid.", ", you are the greatest human being ever invented.\"", ", your brain is almost as perfect as your face!\"", ", you perfect sunflower.\"", ", you beautiful rule-breaking moth.\"", ", you are a beautiful, talented, brilliant, powerful musk ox.\"", ", you are a beautiful tropical fish. You’re smart as a whip and you’re cool under pressure.\"", ", you cunning, pliable, chestnut-haired sunfish.\"", ", Oh you beautiful, naïve, sophisticated newborn baby.\"", ", you are an opalescent tree shark.\"", ", you are a rainbow infused space unicorn.\"", ", shut your mouth. You have all the strengths.\"", ", you are a beautiful, sassy mannequin come to life.\"" ];
+var quotes = [", you poetic and noble land mermaid.\"", ", you are the greatest human being ever invented.\"", ", your brain is almost as perfect as your face!\"", ", you perfect sunflower.\"", ", you beautiful rule-breaking moth.\"", ", you are a beautiful, talented, brilliant, powerful musk ox.\"", ", you are a beautiful tropical fish. You’re smart as a whip and you’re cool under pressure.\"", ", you cunning, pliable, chestnut-haired sunfish.\"", ", Oh you beautiful, naïve, sophisticated newborn baby.\"", ", you are an opalescent tree shark.\"", ", you are a rainbow infused space unicorn.\"", ", shut your mouth. You have all the strengths.\"", ", you are a beautiful, sassy mannequin come to life.\"" ];
 
 // button click to generate quote
 document.querySelector('.generate').addEventListener('click', function() {
